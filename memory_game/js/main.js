0cda88b993f99c8049e6449e9594077dea4ab773
natesena <nate.sena1@gmail.com>
2017-08-17 16:40:33 -0700

    new changes with logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,19 @@
 //main.js cc Nathaniel Sena
 console.log("up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+if(cardsInPlay.length ===2) {
+	if(cardsInPlay[1] === cardsInPlay[2]){
+		alert("You Found a match!");
+	}
+	else {
+		alert("sorry, no match");
+	}
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
