3a7f2862c4de1b3bde8154ebbb3c4a8601e90b05
natesena <nate.sena1@gmail.com>
2017-08-17 18:42:06 -0700

    Added More Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,28 @@
 //main.js cc Nathaniel Sena
-console.log("up and running!");
+//console.log("up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-if(cardsInPlay.length ===2) {
-	if(cardsInPlay[1] === cardsInPlay[2]){
-		alert("You Found a match!");
-	}
+
+var checkForMatch = function(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You Found a match!");
+		}
 	else {
-		alert("sorry, no match");
+		console.log("sorry, no match");
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log(" User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length ===2) {
+		checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(1);
+
+
+
+
 
